5e1471bf97fc30bbd3c00a5ac79f4fbeab9ca2cd
aljazk <aljaz.kozelj@solvera-lynx.com>
2022-03-11 13:31:48 +0100

    Add logging

--- src/main/java/org/test/Hello.java
+++ src/main/java/org/test/Hello.java
@@ -1,16 +1,21 @@
 package org.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
 public class Hello {
+    private static final Logger log = LoggerFactory.getLogger(Hello.class);
 
     private String name;
     private String message;
 
     public void createMessage() {
+        log.info("Name submitted.");
         message = "Hello, " + name + "!";
     }
 
